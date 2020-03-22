f66883ea9a9d54676c633aad92e0016b7ec5bf9a
ManuC <macalambas@gmail.com>
2020-03-21 21:41:39 -0500

    se agrega componente para el manejo de map action

--- vuex-II-MapState/assets/js/vueComponents/hijo.js
+++ vuex-II-MapState/assets/js/vueComponents/hijo.js
@@ -1,19 +1,26 @@
-Vue.component('hijo', {
-    template: /*html*/
-    `
+Vue.component("hijo", {
+    /*html*/
+    template: `
     <div>
         <button 
             class="btn btn-success" 
             @click="aumentar">+</button>
         <button class="btn btn-danger"
             @click="disminuir(2)">-</button>
-        <h1>{{numero}}</h1>
+        <button class="btn btn-info"
+            @click="obtenerPokemon">obtener Pokemon</button>
+        
+            <h1>{{numero}}</h1>
+        <ul v-for="item of pokemon">
+            <li>{{ item.name }}</li>
+        </ul>
     </div>
     `,
     computed: {
-        ...Vuex.mapState(['numero'])
+        ...Vuex.mapState(["numero", "pokemon"])
     },
     methods: {
-        ...Vuex.mapMutations(['aumentar', 'disminuir']),
-    },
-})
+        ...Vuex.mapMutations(["aumentar", "disminuir"]),
+        ...Vuex.mapActions(["obtenerPokemon"])
+    }
+});
--- vuex-II-MapState/assets/js/vueComponents/vuexStore.js
+++ vuex-II-MapState/assets/js/vueComponents/vuexStore.js
@@ -1,6 +1,7 @@
 const store = new Vuex.Store({
     state:{
         numero : 10,
+        pokemon : [],
     },
     mutations:{
         aumentar(state){
@@ -8,6 +9,16 @@ const store = new Vuex.Store({
         },
         disminuir(state, n){
             state.numero-=n;
+        },
+        llenarPokemon(state, pokemonAction){
+            state.pokemon = pokemonAction;
+        }
+    },
+    actions:{
+        obtenerPokemon: async function({commit}){
+            const data = await fetch('https://uinames.com/api/?amount=20');
+            const cursos = await data.json();
+            commit('llenarPokemon', cursos);
         }
     }
 })
